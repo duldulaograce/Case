8ad629a5704de4dc09fcee022c3050ed15c881ba
duldulaograce <graceduldulao487@gmail.com>
2025-09-28 00:31:02 +0800

    commited

---
+++ casestudyv2/page.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mary Grace Duldulao – Portfolio</title>
+  <link rel="stylesheet" href="page.css">
+</head>
+<body>
+
+  <!-- Header / Intro -->
+  <header class="hero">
+    <div class="container">
+      <img src="duls.jpg" alt="Profile Photo" class="profile-pic">
+      <h1>Mary Grace Duldulao</h1>
+      <p class="tagline">Information Technology Student | Student</p>
+      <a href="contact-dul.php" class="btn">Contact Me</a>
+
+    </div>
+  </header>
+
+  
+  <section id="about" class="section">
+    <h2>About Me</h2>
+    <p>
+       A driven and detail-oriented professional with a strong track record in rapid learning and exceptional time
+management skills. Known for a positive attitude, fostering meaningful relationships, and contributing to customer
+ satisfaction in both private and government settings. A student at Tarlac State University. A hardworking and passionate
+ individual seeking an entry-level Part-Time position. Eager to contribute to team success through excellent
+ organizational and communication skills
+    </p>
+  </section>
+
+
+  <section id="education" class="section bg-light">
+    <h2>Education</h2>
+    <ul>
+      <li><strong>Tarlac State University 
+Bachelor of Science in Information Technology majoy in Web and Mobile Application (2025-2026)</li>
+      <li>TESDA Certified (NCII) (2018-2019)</li>
+      <li>Padapada National High School (ICT) (2018-2019)</li>
+    </ul>
+  </section>
+
+  
+  <section id="skills" class="section">
+    <h2>Skills</h2>
+    <div class="skills-grid">
+      <span>Microsoft excel</span>
+      <span>Typing Skill</span>
+      <span>Basic Photography</span>
+      <span>Data Oriented</span>
+      <span>Analytic Thinking</span>
+      <span>Time Management</span>
+    </div>
+  </section>
+
+  <section id="experience" class="section bg-light">
+    <h2>Experience</h2>
+    <p>
+      •  Intern- 
+City Information Office (Tarlac City) (June 2025-July 2025) <br>
+      •  Third Member – Board of Election Inspectors Commission
+ on Elections (COMELEC) – May 2025 <br>
+  •  Intern Data Encoder (My Creative Panda Inc.)-Extracted and compiled audit data
+ from databases (November 2018 to
+ December 2018 ) <br>
+  •  Staff (Commission on Election) (April 2022 to May
+ 2022)
+
+ <footer>
+    <p>&copy; 2025 WMA-4C</p>
+  </footer>
+
+</body>
+</html>
