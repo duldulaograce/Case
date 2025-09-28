4960fcbfc50f2e83f9817332399fa9515e583628
duldulaograce <graceduldulao487@gmail.com>
2025-09-28 20:52:35 +0800

    committed

---
+++ web.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Case Study</title>
+  <link rel="stylesheet" href="web.css">
+</head>
+<body>
+
+  <!-- Header Section -->
+  <header class="hero">
+    <nav class="navbar">
+      <h1 class="logo">Website</h1>
+      
+    </nav>
+
+    <div class="hero-content">
+      <h2>Welcome to our Group Case Study</h2>
+      <p>PRESENTED BY:</p><br></br>
+      <a href="page.php"class="btn">Duldulao</a>
+      <a href="tumang.php"class="btn">Tumang</a>
+      <a href="soliman.php"class="btn">Soliman</a>
+      <a href="Jhiro.php"class="btn">Jhiro</a>
+    </div>
+  </header>
+
+  
+  <section id="features" class="features">
+    <div class="feature-box">
+      <div>
+        <h4>Tarlac State University</h4>
+      </div>
+      <div>
+        <h4>Bachelor of Science in Information Technology</h4>
+      </div>
+      <div>
+        <h4>WMA 4-C</h4>
+      </div>
+    </div>
+  </section>
+
+
+  <footer>
+    <p>&copy; 2025 WMA-4C</p>
+  </footer>
+
+</body>
+</html>
